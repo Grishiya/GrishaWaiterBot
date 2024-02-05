34d64d0ef1193aa4177ec418300c6f802b85f0d7
Grisha <guruwolf8@gmail.com>
2024-02-05 15:31:36 +0300

    Initial commit

---
+++ src/main/java/com/example/GrishaWaiterBot/GrishaWaiterBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrishaWaiterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrishaWaiterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrishaWaiterBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GrishaWaiterBot/GrishaWaiterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GrishaWaiterBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrishaWaiterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
